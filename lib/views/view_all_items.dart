33e28bd0303354b36acb9fda7ba0b20f4834d34d
macbook <gulomnaziribrohimov@gmail.com>
2024-12-03 10:50:59 +0500

    adding view all items

--- lib/views/view_all_items.dart
+++ lib/views/view_all_items.dart
@@ -1,4 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:recipe_app/utils/constants.dart';
+import 'package:recipe_app/widgets/my_icon_button.dart';
 
 class ViewAllItems extends StatefulWidget {
   const ViewAllItems({super.key});
@@ -8,8 +12,44 @@ class ViewAllItems extends StatefulWidget {
 }
 
 class _ViewAllItemsState extends State<ViewAllItems> {
+  final CollectionReference categoriesItems =
+      FirebaseFirestore.instance.collection("Recipe-app");
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: kbackgroundColor,
+      appBar: AppBar(
+        backgroundColor: kbackgroundColor,
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        actions: [
+          const SizedBox(
+            width: 15,
+          ),
+          MyIconButton(
+            icon: Icons.arrow_back_ios,
+            pressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          const Spacer(),
+          const Text(
+            "Quick & Easy",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const Spacer(),
+          MyIconButton(
+            icon: Iconsax.notification,
+            pressed: () {},
+          ),
+          const SizedBox(
+            width: 15,
+          ),
+        ],
+      ),
+    );
   }
 }
