e843af130b3e20bf8975cbe0086209693abbc5f1
macbook <gulomnaziribrohimov@gmail.com>
2024-12-03 08:37:25 +0500

    first commit

---
+++ lib/views/my_app_home_screen.dart
@@ -0,0 +1,217 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:recipe_app/utils/constants.dart';
+import 'package:recipe_app/widgets/banner.dart';
+import 'package:recipe_app/widgets/food_items_display.dart';
+import 'package:recipe_app/widgets/my_icon_button.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class MyAppHomeScreen extends StatefulWidget {
+  const MyAppHomeScreen({super.key});
+
+  @override
+  State<MyAppHomeScreen> createState() => _MyAppHomeScreenState();
+}
+
+class _MyAppHomeScreenState extends State<MyAppHomeScreen> {
+  String category = "All";
+  final CollectionReference categoriesItems =
+      FirebaseFirestore.instance.collection("App-Category");
+  Query get filteredRecipes => FirebaseFirestore.instance
+      .collection("Recipe-app")
+      .where('category', isEqualTo: category);
+  Query get allRecipes => FirebaseFirestore.instance.collection("Recipe-app");
+  Query get selectedRecipe => category == "All" ? allRecipes : filteredRecipes;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kbackgroundColor,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    headerParts(),
+                    mySearchBar(),
+                    // for banner
+                    const BannerToExplore(),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 20),
+                      child: Text(
+                        "Categories",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    // for category
+                    selectedCategory(),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          "Quick & Easy",
+                          style: TextStyle(
+                            fontSize: 20,
+                            letterSpacing: 0.1,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "View all",
+                            style: TextStyle(
+                              color: kBannerColor,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              StreamBuilder(
+                stream: selectedRecipe.snapshots(),
+                builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.hasData) {
+                    final List<DocumentSnapshot> recipes =
+                        snapshot.data?.docs ?? [];
+                    return Padding(
+                      padding: const EdgeInsets.only(
+                        top: 5,
+                        left: 15,
+                      ),
+                      child: SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                          children: recipes
+                              .map(
+                                (e) => FoodItemsDisplay(documentSnapshot: e),
+                              )
+                              .toList(),
+                        ),
+                      ),
+                    );
+                  }
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  StreamBuilder<QuerySnapshot<Object?>> selectedCategory() {
+    return StreamBuilder(
+      stream: categoriesItems.snapshots(),
+      builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+        if (streamSnapshot.hasData) {
+          return SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+                children: List.generate(
+              streamSnapshot.data!.docs.length,
+              (index) => GestureDetector(
+                onTap: () {
+                  setState(() {
+                    category = streamSnapshot.data!.docs[index]["name"];
+                  });
+                },
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 20,
+                  ),
+                  margin: const EdgeInsets.only(right: 20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                    color: category == streamSnapshot.data!.docs[index]["name"]
+                        ? kprimaryColor
+                        : Colors.white,
+                  ),
+                  child: Text(
+                    streamSnapshot.data!.docs[index]["name"],
+                    style: TextStyle(
+                      color:
+                          category == streamSnapshot.data!.docs[index]["name"]
+                              ? Colors.white
+                              : Colors.grey.shade600,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+            )),
+          );
+        }
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+  }
+
+  Padding mySearchBar() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 22),
+      child: TextField(
+        decoration: InputDecoration(
+          filled: true,
+          prefixIcon: const Icon(Iconsax.search_normal),
+          fillColor: Colors.white,
+          border: InputBorder.none,
+          hintText: "Search any recipes",
+          hintStyle: const TextStyle(
+            color: Colors.grey,
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide.none,
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide.none,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Row headerParts() {
+    return Row(
+      children: [
+        const Text(
+          "What are\ncooking today?",
+          style: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+            height: 1,
+          ),
+        ),
+        const Spacer(),
+        MyIconButton(
+          icon: Iconsax.notification,
+          pressed: () {},
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/views/view_all_items.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ViewAllItems extends StatefulWidget {
+  const ViewAllItems({super.key});
+
+  @override
+  State<ViewAllItems> createState() => _ViewAllItemsState();
+}
+
+class _ViewAllItemsState extends State<ViewAllItems> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
